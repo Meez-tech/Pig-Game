6e092984827301c08f2419f429f749bd52b9d7a7
Meez Dev <66494396+Meez-tech@users.noreply.github.com>
2020-11-12 15:13:44 +0400

    Update Code
    Added gamePlaying variable

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 //manipulate DOM
 //read from DOM
 //change CSS styles
-var scores, roundScore, activePlayer;
+//Only declaring variables, no need to define
+var scores, roundScore, activePlayer, gamePlaying;
 init();
 
 document.getElementById('score--0').textContent = '0';
@@ -51,63 +52,77 @@ function nextPlayer() {
 //no need to add (), we want EL to call it, and not to be automatically
 //anonymous function with no name, can not be reused outside this context
 document.querySelector('.btn--roll').addEventListener('click', function () {
-  //1. a random number as soon as someone clicks
-  var dice = Math.floor(Math.random() * 6) + 1;
-  //2. display the result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
+  if (gamePlaying) {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //2. display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-  //3.update the round score IF the rolled number is not a 1
-  if (dice !== 1) {
-    //Addscore
-    roundScore += dice;
-    document.querySelector(
-      '#current--' + activePlayer
-    ).textContent = roundScore;
-  } else {
-    //Next Player
-    nextPlayer();
-    // document.querySelector('#current--' + activePlayer).textContent = '0';
+    //3.update the round score IF the rolled number is not a 1
+    if (dice !== 1) {
+      //Addscore
+      roundScore += dice;
+      document.querySelector(
+        '#current--' + activePlayer
+      ).textContent = roundScore;
+    } else {
+      //Next Player
+      nextPlayer();
+      // document.querySelector('#current--' + activePlayer).textContent = '0';
+    }
   }
+  //1. a random number as soon as someone clicks
 });
 
 //use function correctly to apply DRY
 //Think about game logic as a programer
 document.querySelector('.btn--hold').addEventListener('click', function () {
-  //Add player current score to golabl score
-  scores[activePlayer] += roundScore;
+  if (gamePlaying) {
+    //Add player current score to golabl score
+    scores[activePlayer] += roundScore;
 
-  //update UI
-  document.querySelector('#score--' + activePlayer).textContent =
-    scores[activePlayer];
+    //update UI
+    document.querySelector('#score--' + activePlayer).textContent =
+      scores[activePlayer];
 
-  //check if player won the game
-  if (scores[activePlayer] >= 10) {
-    document.querySelector('#name--' + activePlayer).textContent = 'Winner';
-    document.querySelector('.dice').style.display = 'none';
-    document
-      .querySelector('.player--' + activePlayer)
-      .classList.add('player--winner');
-    document
-      .querySelector('.player--' + activePlayer)
-      .classList.remove('player--active');
-  } else {
-    //next player
-    nextPlayer();
+    //check if player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name--' + activePlayer).textContent = 'Winner';
+      document.querySelector('.dice').style.display = 'none';
+      document
+        .querySelector('.player--' + activePlayer)
+        .classList.add('player--winner');
+      document
+        .querySelector('.player--' + activePlayer)
+        .classList.remove('player--active');
+      gamePlaying = false;
+    } else {
+      //next player
+      nextPlayer();
+    }
   }
 });
 
 //New Game
 document.querySelector('.btn--new').addEventListener('click', init);
 
+//Init. the game, reset everything
 function init() {
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0;
+  gamePlaying = true;
   document.querySelector('.dice').style.display = ' none';
   document.getElementById('score--0').textContent = '0';
   document.getElementById('score--1').textContent = '0';
   document.getElementById('current--0').textContent = '0';
   document.getElementById('current--1').textContent = '0';
+  document.getElementById('name--0').textContent = 'Player 1';
+  document.getElementById('name--1').textContent = 'Player 2';
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner');
+  document.querySelector('.player--0').classList.remove('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
+  document.querySelector('.player--0').classList.add('player--active');
 }
